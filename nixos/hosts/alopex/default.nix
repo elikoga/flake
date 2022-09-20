d66c37a25fdfcd2439e040d8fce4dbe0489080e6
Eli Kogan-Wang <elikowa@gmail.com>
2022-09-20 21:04:06 +0000

    add netdata, nginx

--- nixos/hosts/alopex/default.nix
+++ nixos/hosts/alopex/default.nix
@@ -12,6 +12,36 @@ in
 
   services.openssh.enable = true;
   services.vscode-server.enable = true;
+  services.netdata.enable = true;
+  services.nginx = {
+    recommendedOptimisation = true;
+    recommendedTlsSettings = true;
+    recommendedGzipSettings = true;
+    recommendedProxySettings = true;
+    virtualHosts =
+      let
+        base = {
+          forceSSL = true;
+          enableACME = true;
+        };
+        rootBase = root: base // {
+          locations."/" = root;
+        };
+      in
+      {
+        "alopex.6xr.de" = rootBase {
+          return = "302 https://eliko.ga/";
+        };
+        "netdata.alopex.6xr.de" = rootBase {
+          proxyPass = "http://localhost:19999/";
+        };
+      };
+  };
+
+  security.acme = {
+    acceptTerms = true;
+    email = "elikowa" + "@" + "gmail.com";
+  };
 
   users.users.elikoga = {
     openssh.authorizedKeys.keys = keyfiles.elikoga;
@@ -20,15 +50,16 @@ in
   users.users.root.openssh.authorizedKeys.keys = keyfiles.elikoga;
   users.mutableUsers = false;
 
-  networking.hostName = "alopex";
+  networking.hostName = " alopex ";
 
   networking.defaultGateway6 = {
-    address = "fe80::1";
-    interface = "ens18";
+    address = " fe80::1 ";
+    interface = "
+              ens18 ";
   };
 
   networking.interfaces.ens18.ipv6.addresses = [{
-    address = "2a02:c206:3009:7843::1";
+    address = " 2 a02:c206:3009:7843::1 ";
     prefixLength = 64;
   }];
 
@@ -41,9 +72,11 @@ in
   ];
 
   environment.variables = {
-    EDITOR = "nvim";
+    EDITOR = "
+              nvim ";
   };
 
   # The comment: Read the changelogs.
-  system.stateVersion = "22.05"; # Did you read the comment?
+  system.stateVersion = " 22.05 "; # Did you read the comment?
 }
+
