5108224d9dd36d0e37813bd18d2a05bd266311c7
Eli Kogan-Wang <elikowa@gmail.com>
2022-09-20 19:49:38 +0000

    alopex: add ipv6

--- nixos/hosts/alopex/default.nix
+++ nixos/hosts/alopex/default.nix
@@ -21,6 +21,16 @@ in
 
   networking.hostName = "alopex";
 
+  networking.defaultGateway6 = {
+    address = "fe80::1";
+    interface = "ens18";
+  };
+
+  networking.interfaces.ens18.ipv6.addresses = [{
+    address = "2a02:c206:3009:7843::1";
+    prefixLength = 64;
+  }];
+
   # The comment: Read the changelogs.
   system.stateVersion = "22.05"; # Did you read the comment?
 }
