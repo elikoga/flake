5196fbc379d97d53985826f9009f193d58a2d7df
Eli Kogan-Wang <elikowa@gmail.com>
2022-09-20 19:26:26 +0000

    alopex: nvm remove dhcpD!

--- nixos/hosts/alopex/default.nix
+++ nixos/hosts/alopex/default.nix
@@ -11,7 +11,6 @@ in
 
   services.openssh.enable = true;
   services.vscode-server.enable = true;
-  services.dhcpd6.enable = true;
 
   users.users.elikoga = {
     openssh.authorizedKeys.keys = keyfiles.elikoga;
