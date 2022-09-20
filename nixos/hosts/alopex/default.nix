9b648a4ca0b73cf831d8b211f6fce6161f0749b7
Eli Kogan-Wang <elikowa@gmail.com>
2022-09-20 21:10:55 +0000

    enable nginx

--- nixos/hosts/alopex/default.nix
+++ nixos/hosts/alopex/default.nix
@@ -15,6 +15,7 @@ in
   services.vscode-server.enable = true;
   services.netdata.enable = true;
   services.nginx = {
+    enable = true;
     recommendedOptimisation = true;
     recommendedTlsSettings = true;
     recommendedGzipSettings = true;
